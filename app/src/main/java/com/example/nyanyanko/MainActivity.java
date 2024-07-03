a537d157267f79c5233d299ff7f568a15b05950a
CIIT601PC06\Student <it@ciit.edu.ph>
2024-07-03 12:00:33 +0800

    initial commit

---
+++ app/src/main/java/com/example/nyanyanko/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.nyanyanko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button next_Activity_button = (Button) findViewById(R.id.first_activity_button);
+
+        next_Activity_button.setOnClickListener(v ->
+        {
+            Intent intent = new Intent(MainActivity.this, Registration.class);
+            startActivity(intent);
+        });
+    }
+}
